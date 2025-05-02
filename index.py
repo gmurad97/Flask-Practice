be30e7d9ef678b31c17407332e0c622fc30e9067
Murad Gazymagomedov <murad.dev@bk.ru>
2025-05-02 23:31:18 +0400

    context to page| for loop

--- index.py
+++ index.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def index():
-    return render_template("index.html",title="Home")
+    context = {
+        "menus": [
+            "Home",
+            "About",
+            "Contact",
+            "Shop",
+            "Blog",
+            "Films",
+            "Videos",
+        ],
+    }
+    return render_template("index.html", **context)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html",title="About")
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
